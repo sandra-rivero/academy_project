b90b33c4d9c0b7e5c4d6d23523ce991639241b60
Sandra Rivero <sandrarivero305@gmail.com>
2025-03-13 17:06:01 +0100

    I add new features, such ad deleting a workshop from the academy, adding and removing sutdents or teachers froom the workshops

--- academy_project.py
+++ academy_project.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 class Academy:
     DANCE_STYLES = ["salsa","bachata","sevillanas","ballet","hip-hop", "kizomba","Pasos libres"]
@@ -16,9 +17,11 @@ class Academy:
         return (f"Academy [{self.name}]: dance_styles={self.dance_styles}")
     
     def add_dance_style(self, dance_style):
-        #Before adding a dance style, we verify that it is a correct style, then we check whether it has already been added or not
+    #Before adding a dance style, we verify that it is a correct style, then we check whether it has already been added or not
         if dance_style not in self.DANCE_STYLES:
             raise ValueError("Must be dance style")
+        if dance_style in self.prohibited_dances:
+            raise ValueError("Be careful, this dance style is prohibited")
         if not dance_style in self.dance_styles:
             self.dance_styles.append(dance_style)
         else:
@@ -40,11 +43,19 @@ class Academy:
         return self.courses
     
     def add_workshop(self,new_workshop):
+    #Add a workshop from the academy
         if new_workshop not in self.workshops:
             self.workshops.append(new_workshop)
             print("Workshop added.")
         else:
             raise ValueError(f"{new_workshop} already exists.")  
+        
+    def delete_workshop(self, workshop):
+    #Delete a workshop from the academy
+        if workshop in self.workshops:
+            self.workshops.remove(workshop)
+        else:
+            raise ValueError(f"Workshop {workshop} not found")    
 
     def add_student(self,student):
         if student not in self.students:
@@ -64,7 +75,7 @@ class DanceStyle:
         return self.style
 
     def add_dance_style(self,style):
-        if style not in self.DANCE_STYLES:
+        if style not in self.dance_styles:
             self.dance_style.append(style)
         else:
             raise ValueError(f"The dance style {style} is already added.")
@@ -81,7 +92,7 @@ class Student:
          self.phone = phone
 
     def __str__ (self):
-        return f"{self.name}"     
+        return f"{self.name}"    
 
     def change_name(self, new_name):
         self.name = new_name
@@ -118,13 +129,20 @@ class Course:
         self.max_students = max_students
 
     def __str__(self):
-        return f"{self.dance_style}, {self.teacher}"  
+        return f"{self.dance_style}, {self.teachers}"  
 
     def add_student(self, new_student):
         if len(self.students) < self.max_students:
             self.students.append(new_student)
         else:
             raise ValueError("Class is full, you cannot add more students")  
+
+    def delete_student(self,student):
+    #Remove a student from the course
+        if student in self.students:
+            self.students.remove(student) 
+        else:
+            raise ValueError (f"The student {student} is not found")      
    
     def show_students(self):
         return self.students
@@ -148,6 +166,44 @@ class Workshop:
             raise ValueError("Class is full, you cannot add more students")
 
     def show_students(self):
-        return self.students           
+        return self.students   
+
+    def delete_teacher(self,teacher):
+    #Remove a teacher from the workshop
+        if teacher in self.teachers:
+            self.teachers.remove(teacher) 
+        else:
+            raise ValueError (f"The teacher {teacher} is not found")   
+
+    def delete_student(self,student):
+    #Remove a student from the workshop
+        if student in self.students:
+            self.students.remove(student) 
+        else:
+            raise ValueError (f"The student {student} is not found")         
+    '''
     
- 
+ #Test to add a dance style to our academy and show them
+academy = Academy("Victorys")
+academy.add_dance_style("Pasos libres")
+academy.add_dance_style("bachata")
+academy.add_dance_style("salsa")
+academy.add_dance_style("sevillanas")
+print(academy.dance_styles)
+#Test to change the name of the academy and show them
+academy.change_name("Infinit")
+print(academy.name)
+#Test to add a course and show them
+academy.add_course("Bachata inicio 1")
+academy.add_course("Bachata medio 1")
+academy.add_course("Bachata medio 2")
+print(academy.courses)
+#Test to add a student, and show them
+academy.add_student("Pepe")
+academy.add_student("Maria")
+academy.add_student("Noelia")
+academy.add_student("Luis")
+print(academy.students)
+#Test to see if the prohibited dance can be added or not
+academy.add_dance_style("kizomba")'
+'''''
